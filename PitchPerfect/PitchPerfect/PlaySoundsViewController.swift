5183b3a280bc5ff9bd2336feaadd21ea14c79dd2
Peke! <pekelike@gmail.com>
2015-10-04 23:17:01 -0700

    Lesson 4 - Part b
    - Added chipmunk effect (higher pitch)
    - Added DarthVader effect (lower pitch)

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -13,6 +13,8 @@ class PlaySoundsViewController: UIViewController {
     
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,8 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,8 +58,38 @@ class PlaySoundsViewController: UIViewController {
         playSound(1.5, startFromTop:true)
     }
     
+    @IBAction func onPlayChipmunk(sender: UIButton) {
+        playSoundWithVariablePitch(1000)
+    }
+    
+    @IBAction func onPlayDarth(sender: UIButton) {
+        playSoundWithVariablePitch(-1000)
+    }
+    
     @IBAction func onStop(sender: UIButton) {
         audioStop()
+    }
+    
+    func playSoundWithVariablePitch(pitch: Float) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to:changePitchEffect, format:nil)
+        audioEngine.connect(changePitchEffect, to:audioEngine.outputNode, format:nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+
     }
     
     func playSound(speedOfPlay: Float, startFromTop: Bool) {
